b1a3a4aa1bd1420bf9522967549df0e3c26a4a0d
TD <dn.timur@gmail.com>
2024-02-27 00:19:59 +0200

    ignored files, added angle mechanism for barrel and shooting mechanism as well

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 install:
-	pip install -r requirements.txt
+	./game_venv/bin/pip3 install -r requirements.txt
 run:
 	python3 ~/work/personal_stuff/game_proj/main.py
--- src/Bullet.py
+++ src/Bullet.py
@@ -4,19 +4,24 @@ from pygame import Vector2
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, angle, barrel_pos, bullet_image):
+    def __init__(self, barrel_height, angle, barrel_pos, bullet_image):
         super().__init__()
-        self.angle = -(180 - angle)
+        self.angle = angle
         self.pos = Vector2(barrel_pos[0], barrel_pos[1])
 
         self.image_origin = pygame.image.load(bullet_image)
         self.image = self.image_origin
+        self.image_height = self.image.get_height()
 
-        self.image = pygame.transform.rotate(self.image_origin, self.angle)
+        self.image = pygame.transform.rotate(self.image_origin, self.angle + 180)
+        self.pos.x += (barrel_height/2 + self.image_height/2) * math.sin(math.radians(self.angle))
+        self.pos.y += (barrel_height/2 + self.image_height/2) * math.cos(math.radians(self.angle))
         self.rect = self.image.get_rect(center=self.pos)
 
+        self.shoot_sprites = []
+
     def update(self):
-        self.pos.x = self.pos.x + (3*math.cos(math.radians(self.angle)))
-        self.pos.y = self.pos.y + (3*math.sin(math.radians(self.angle)))
+        self.pos.x += 10 * math.sin(math.radians(self.angle))
+        self.pos.y += 10 * math.cos(math.radians(self.angle))
 
         self.rect = self.image.get_rect(center=self.pos)
